117ffcbfd90e1181b375d905054046c8024038e6
Dgarrido21 <dgarrido21@alumnos.utalca.cl>
2023-03-16 21:00:29 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿int cargamax = 2500; //Modificar para escoger cuanto tarda en cargar
+for (double carga = 0; carga <= cargamax; carga++)
+{
+    Console.WriteLine($"Carga al {(int)(carga / cargamax * 100)}%");
+    Console.Write("|");
+    for (int i = 1; i <= (100 / 5); i++)
+    {
+        if ((int)(carga / cargamax * 100) >= i * (100 / 20))
+        {
+            Console.Write("■");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.Write("|\n\n");
+}
